8c8bd5489bd8ebbf32a3455aa71fb8f27986e703
Giovanni Castiglioni <gcastiglioni@whit-e.eu>
2023-10-10 18:01:50 +0200

    fix basic auth to work with namespaces

--- rswag-ui/lib/rswag/ui/basic_auth.rb
+++ rswag-ui/lib/rswag/ui/basic_auth.rb
@@ -17,14 +17,8 @@ module Rswag
       private
 
       def env_matching_path(env)
-        path = base_path(env['PATH_INFO'])
-        Rswag::Ui.config.config_object[:urls].find do |endpoint|
-          base_path(endpoint[:url]) == path
-        end
-      end
-
-      def base_path(url)
-        url.downcase.split('/')[1]
+        path = URI.parse(env['PATH_INFO']).path
+        Rswag::Ui.config.config_object[:urls].find { |endpoint| /#{endpoint[:url]}/ =~ path }
       end
     end
   end
